85ba91cefcec40dfc496979491505b125cb7d6e4
Ruohai <1132370844@qq.com>
2024-02-27 17:33:59 +0800

    lab3 题1

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,24 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+_vmprintf(pagetable_t pagetable, int level){
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) != 0){
+      for(int j = 0; j < level; j++){
+        printf(".. ");
+      }
+      printf("..%d: pte %p pa %p\n", i , pte, PTE2PA(pte));
+     if(level < 2){ uint64 child = PTE2PA(pte);
+      _vmprintf((pagetable_t)child, level+1);}
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  _vmprintf(pagetable,0);
+}
